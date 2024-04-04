2a9b7a8f59bdc841ab79fd2e191bd6a152c60fa3
Luis <lu.ca.oliveira41@gmail.com>
2024-04-03 23:27:07 -0400

    chore: registry bookService on application

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using BookStore.Models;
+using BookStore.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddDbContext<BookStoreContext>(opt =>
 
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<BookService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
